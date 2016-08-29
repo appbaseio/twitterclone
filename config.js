719d42f118b9251157d200ab63f6179d78c026e9
Stanley Sathler <stanleysathlerpinto@gmail.com>
2016-08-29 13:40:16 -0300

    Changed tab to spaces

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Here goes your appbase credentials
  **/
 var credentials = {
-	url: 'https://scalr.api.appbase.io',
+    url: 'https://scalr.api.appbase.io',
     appname: 'twitter',
     username: '6HVI9SUIr',
     password: 'e601d171-dd83-48f0-8e8a-1cf06414ce69'
